df98ad5eec2a096606701f0cdfd6205d2708ed8f
foysal <mdfoysalislam0895@gmail.com>
2024-11-25 23:15:00 +0600

    first commit

---
+++ Test.java
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 5, b = 10, temp;
+
+    cout << "Before swapping." << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << "\nAfter swapping the code ." << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+// here  i  am cheanging my code frome here 
+    return 0;
+}
